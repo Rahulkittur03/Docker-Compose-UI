5c680d42dc0ed8ab22c6877ee5788f6f73fa9163
akumarm642 <133037034+akumarm642@users.noreply.github.com>
2025-07-05 18:08:55 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000, // Change this to your desired port
+    port: 8080, // Change this to your desired port
     // host: '0.0.0.0', // Optional: If you want to expose it to your network
     // strictPort: true, // Optional: Exit if the port is already in use
   },
